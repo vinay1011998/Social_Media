813ddcb89c4de080d30291f0dc7647f73b734c20
unknown <vinay1011998@gmail.com>
2021-04-24 21:58:38 +0530

    Added view engine ejs

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = 8000;
 //Use express router from router folders index.js
 
 app.use('/',require('./routes')); //middleware used when a server starts(before) it need to access this route.
+app.set('view-engine','ejs');
+app.set('views','./views');
 
 
 app.listen(port,function(err)   //listen to requests on port 8000
